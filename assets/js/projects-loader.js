98a787e0f970aecb26c6c759b4227b0a7841d926
PV INCONS <pvincons@gmail.com>
2026-08-15 16:10:34 +0700

    fgg

--- assets/js/projects-loader.js
+++ assets/js/projects-loader.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
   // 1. Tìm container chứa danh sách dự án
-  const projectsContainer = document.getElementById('content/post-projects.js');
+  const projectsContainer = document.getElementById('content/posts-news.json');
   if (!projectsContainer) return;
 
   try {
